11e65f8e94dbd847df85c3c56e163347bc56131c
egoroff <devnull@localhost>
2009-04-27 06:09:47 +0000

    All public members documented by using XML docs.

--- src/MSBuild.TeamCity.Tasks/PublishArtifacts.cs
+++ src/MSBuild.TeamCity.Tasks/PublishArtifacts.cs
@@ -9,11 +9,29 @@ using Microsoft.Build.Utilities;
 
 namespace MSBuild.TeamCity.Tasks
 {
+	/// <summary>
+    /// Publish (upload) artifact(s) to JetBrains TeamCity server
+    /// </summary>
+    /// <example>Publish several artifacts.
+    /// <code><![CDATA[
+    /// <PublishArtifacts Artifacts="File1.zip;File2.zip" />
+    /// ]]></code>
+    /// </example>
 	public class PublishArtifacts : Task
 	{
+		/// <summary>
+        /// Gets or sets the artifacts to publish.
+        /// </summary>
+        /// <value>The artifacts.</value>
 		[Required]
 		public ITaskItem[] Artifacts { get; set; }
 
+		/// <summary>
+        /// When overridden in a derived class, executes the task.
+        /// </summary>
+        /// <returns>
+        /// true if the task successfully executed; otherwise, false.
+        /// </returns>
 		public override bool Execute()
 		{
 			foreach ( ITaskItem item in Artifacts )

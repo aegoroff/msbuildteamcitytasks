e75bf5e935e54c3f20a8a31a82d62b6c7c7dc691
egoroff <devnull@localhost>
2009-05-02 10:47:02 +0000

    Block opening and closing TeamCity messages implemented.

---
+++ MSBuild.TeamCity.Tasks/BlockClose.cs
@@ -0,0 +1,36 @@
+/*
+ * Created by: egr
+ * Created at: 02.05.2009
+ * © 2007-2009 Alexander Egorov
+ */
+
+namespace MSBuild.TeamCity.Tasks
+{
+	///<summary>
+	/// Block closing message. Blocks are used to group several messages in the build log.
+	///</summary>
+	/// <example>Closes block
+	/// <code><![CDATA[
+	/// <BlockClose Name="b1" />
+	/// ]]></code>
+	/// Closes block full example (with all optional attributes)
+	/// <code><![CDATA[
+	/// <BlockClose
+	///		IsAddTimestamp="true"
+	///		FlowId="1"
+	///		Name="b1"
+	/// />
+	/// ]]></code>
+	/// </example>
+	public class BlockClose : BlockTask
+	{
+		/// <summary>
+		/// Creates concrete message class
+		/// </summary>
+		/// <returns></returns>
+		protected override BlockTeamCityMessage CreateMessage()
+		{
+			return new BlockCloseTeamCityMessage(Name);
+		}
+	}
+}
---
+++ MSBuild.TeamCity.Tasks/BlockOpen.cs
@@ -0,0 +1,36 @@
+/*
+ * Created by: egr
+ * Created at: 02.05.2009
+ * © 2007-2009 Alexander Egorov
+ */
+
+namespace MSBuild.TeamCity.Tasks
+{
+	///<summary>
+	/// Block opening message. Blocks are used to group several messages in the build log.
+	///</summary>
+	/// <example>Opens block
+	/// <code><![CDATA[
+	/// <BlockOpen Name="b1" />
+	/// ]]></code>
+	/// Opens block full example (with all optional attributes)
+	/// <code><![CDATA[
+	/// <BlockOpen
+	///		IsAddTimestamp="true"
+	///		FlowId="1"
+	///		Name="b1"
+	/// />
+	/// ]]></code>
+	/// </example>
+	public class BlockOpen : BlockTask
+	{
+		/// <summary>
+		/// Creates concrete message class
+		/// </summary>
+		/// <returns></returns>
+		protected override BlockTeamCityMessage CreateMessage()
+		{
+			return new BlockOpenTeamCityMessage(Name);
+		}
+	}
+}

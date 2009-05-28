05a503fdbd9ba6fad2caada8658c01c4a98cda6c
egoroff <devnull@localhost>
2009-05-28 02:38:28 +0000

    Google tests importing implemented using ImportData message and this task will fail build in case of any failure occured.

--- src/Tests/TGoogleTestXmlReader.cs
+++ src/Tests/TGoogleTestXmlReader.cs
@@ -4,6 +4,7 @@
  * © 2007-2009 Alexander Egorov
  */
 
+using System.IO;
 using MSBuild.TeamCity.Tasks;
 using NUnit.Framework;
 
@@ -14,55 +15,57 @@ namespace Tests
 	{
 		private const string SimpleTestResult =
 			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite tests=\"26\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0.016\" name=\"AllTests\"></testsuite>";
+
 		private const string OneSuiteAndTestTestResult =
 			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite tests=\"1\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0.016\" name=\"AllTests\"><testsuite name=\"suite1\" tests=\"1\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite1\" /></testsuite></testsuite>";
-		
+
 		private const string TwoSuiteAndTwoTestTestResult =
 			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite tests=\"4\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0.016\" name=\"AllTests\"><testsuite name=\"suite1\" tests=\"2\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite1\" /><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite1\" /></testsuite><testsuite name=\"suite2\" tests=\"2\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite2\" /><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite2\" /></testsuite></testsuite>";
-		
-		private const string TwoSuiteAndTwoTestTestOneFailedResult =
-			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite tests=\"4\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0.016\" name=\"AllTests\"><testsuite name=\"suite1\" tests=\"2\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite1\" ><failure message=\"m2\" type=\"\">d2</failure></testcase><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite1\" /></testsuite><testsuite name=\"suite2\" tests=\"2\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite2\" /><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite2\" /></testsuite></testsuite>";
 
-		private const string Suite1Start = "##teamcity[testSuiteStarted name='suite1']";
-		private const string Suite2Start = "##teamcity[testSuiteStarted name='suite2']";
-		private const string Suite1Finish = "##teamcity[testSuiteFinished name='suite1']";
-		private const string Suite2Finish = "##teamcity[testSuiteFinished name='suite2']";
-		private const string Test1Start = "##teamcity[testStarted name='test1']";
-		private const string Test1Failed = "##teamcity[testFailed name='test1' message='m2' details='d2']";
-		private const string Test2Start = "##teamcity[testStarted name='test2']";
-		private const string Test1Finish = "##teamcity[testFinished name='test1' duration='16']";
-		private const string Test2Finish = "##teamcity[testFinished name='test2' duration='16']";
+		private const string TwoSuiteAndTwoTestTestOneFailedResult =
+			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite tests=\"4\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0.016\" name=\"AllTests\"><testsuite name=\"suite1\" tests=\"2\" failures=\"1\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite1\" ><failure message=\"m2\" type=\"\">d2</failure></testcase><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite1\" /></testsuite><testsuite name=\"suite2\" tests=\"2\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite2\" /><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite2\" /></testsuite></testsuite>";
 		
+		private const string TwoSuiteAndTwoTestTestTwoFailedResult =
+			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testsuite tests=\"4\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0.016\" name=\"AllTests\"><testsuite name=\"suite1\" tests=\"2\" failures=\"2\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite1\" ><failure message=\"m2\" type=\"\">d2</failure><failure message=\"m3\" type=\"\">d3</failure></testcase><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite1\" /></testsuite><testsuite name=\"suite2\" tests=\"2\" failures=\"0\" disabled=\"0\" errors=\"0\" time=\"0\"><testcase name=\"test1\" status=\"run\" time=\"0.016\" classname=\"suite2\" /><testcase name=\"test2\" status=\"run\" time=\"0.016\" classname=\"suite2\" /></testsuite></testsuite>";
+
 		[Test]
 		public void ReadEmpty()
 		{
-			GoogleTestXmlReader reader = new GoogleTestXmlReader(SimpleTestResult);
-			Assert.That(reader.Read(), Is.Empty);
+			GoogleTestXmlReader reader = new GoogleTestXmlReader(new StringReader(SimpleTestResult));
+			reader.Read();
 			Assert.That(reader.FailuresCount, Is.EqualTo(0));
 		}
-		
+
 		[Test]
 		public void ReadOneSuiteAndTest()
 		{
-			GoogleTestXmlReader reader = new GoogleTestXmlReader(OneSuiteAndTestTestResult);
-			Assert.That(reader.Read(), Is.EquivalentTo(new[] { Suite1Start, Test1Start, Test1Finish, Suite1Finish }));
+			GoogleTestXmlReader reader = new GoogleTestXmlReader(new StringReader(OneSuiteAndTestTestResult));
+			reader.Read();
 			Assert.That(reader.FailuresCount, Is.EqualTo(0));
 		}
-		
+
 		[Test]
 		public void ReadTwoSuiteAndTwoTest()
 		{
-			GoogleTestXmlReader reader = new GoogleTestXmlReader(TwoSuiteAndTwoTestTestResult);
-			Assert.That(reader.Read(), Is.EquivalentTo(new[] { Suite1Start, Test1Start, Test1Finish, Test2Start, Test2Finish, Suite1Finish, Suite2Start, Test1Start, Test1Finish, Test2Start, Test2Finish, Suite2Finish }));
+			GoogleTestXmlReader reader = new GoogleTestXmlReader(new StringReader(TwoSuiteAndTwoTestTestResult));
+			reader.Read();
 			Assert.That(reader.FailuresCount, Is.EqualTo(0));
 		}
-		
+
 		[Test]
 		public void ReadTwoSuiteAndTwoTestOneFailed()
 		{
-			GoogleTestXmlReader reader = new GoogleTestXmlReader(TwoSuiteAndTwoTestTestOneFailedResult);
-			Assert.That(reader.Read(), Is.EquivalentTo(new[] { Suite1Start, Test1Start, Test1Failed, Test1Finish, Test2Start, Test2Finish, Suite1Finish, Suite2Start, Test1Start, Test1Finish, Test2Start, Test2Finish, Suite2Finish }));
+			GoogleTestXmlReader reader = new GoogleTestXmlReader(new StringReader(TwoSuiteAndTwoTestTestOneFailedResult));
+			reader.Read();
 			Assert.That(reader.FailuresCount, Is.EqualTo(1));
+		}
+		
+		[Test]
+		public void ReadTwoSuiteAndTwoTestTwoFailures()
+		{
+			GoogleTestXmlReader reader = new GoogleTestXmlReader(new StringReader(TwoSuiteAndTwoTestTestTwoFailedResult));
+			reader.Read();
+			Assert.That(reader.FailuresCount, Is.EqualTo(2));
 		}
 	}
 }

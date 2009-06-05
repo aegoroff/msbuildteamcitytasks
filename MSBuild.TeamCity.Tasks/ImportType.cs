404dd0ccb2cabd83ee0d0830218d4f36103e3a40
egoroff <devnull@localhost>
2009-06-05 21:32:39 +0000

    ImportType enumeration introduced to decrease possible bad report type setting.

---
+++ MSBuild.TeamCity.Tasks/ImportType.cs
@@ -0,0 +1,44 @@
+/*
+ * Created by: egr
+ * Created at: 06.06.2009
+ * © 2007-2009 Alexander Egorov
+ */
+
+namespace MSBuild.TeamCity.Tasks
+{
+	/// <summary>
+	/// Represents Team City import report types
+	/// </summary>
+	public enum ImportType
+	{
+		/// <summary>
+		/// Represents JUnit report "junit"
+		/// </summary>
+		Junit,
+		
+		/// <summary>
+		/// Represents Surefire report "surefire"
+		/// </summary>
+		Surefire,
+		
+		/// <summary>
+		/// Represents NUnit report "nunit"
+		/// </summary>
+		Nunit,
+		
+		/// <summary>
+		/// Represents FindBugs report "findBugs"
+		/// </summary>
+		FindBugs,
+		
+		/// <summary>
+		/// Represents PMD report "pmd"
+		/// </summary>
+		Pmd,
+		
+		/// <summary>
+		/// Represents FxCop report "FxCop"
+		/// </summary>
+		FxCop,
+	}
+}

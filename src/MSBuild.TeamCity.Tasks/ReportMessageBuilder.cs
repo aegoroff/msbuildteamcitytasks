2baae53e3255f756deac06c190e31a6a9f1dbb14
egoroff <devnull@localhost>
2010-09-02 08:52:29 +0000

    ReportMessage class refactored to improve unit testing. ReportMessageBuilder class extracted to test business logic.

---
+++ src/MSBuild.TeamCity.Tasks/ReportMessageBuilder.cs
@@ -0,0 +1,47 @@
+/*
+ * Created by: egr
+ * Created at: 02.09.2010
+ * © 2007-2010 Alexander Egorov
+ */
+
+namespace MSBuild.TeamCity.Tasks
+{
+	/// <summary>
+	/// Represents <see cref="ReportMessageTeamCityMessage"/> instance builder
+	/// </summary>
+	public class ReportMessageBuilder
+	{
+		private readonly string _text;
+		private readonly string _status;
+		private readonly string _details;
+
+		///<summary>
+		/// Initializes a new instance of the <see cref="ReportMessageBuilder"/> class
+		///</summary>
+		///<param name="text">Message text</param>
+		///<param name="status">The status attribute may take following values: NORMAL, WARNING, FAILURE, ERROR. The default value is NORMAL.</param>
+		///<param name="details">error details text that is used only if status is ERROR</param>
+		public ReportMessageBuilder(string text, string status, string details)
+		{
+			_text = text;
+			_status = status;
+			_details = details;
+		}
+
+		/// <summary>
+		/// Builds 
+		/// </summary>
+		public TeamCityMessage BuildMessage()
+		{
+			if ( !string.IsNullOrEmpty(_status) && !string.IsNullOrEmpty(_details) )
+			{
+				return new ReportMessageTeamCityMessage(_text, _status, _details);
+			}
+			if ( !string.IsNullOrEmpty(_status) )
+			{
+				return new ReportMessageTeamCityMessage(_text, _status);
+			}
+			return new ReportMessageTeamCityMessage(_text);
+		}
+	}
+}
---
+++ src/Tests/TReportMessageBuilder.cs
@@ -0,0 +1,56 @@
+/*
+ * Created by: egr
+ * Created at: 02.09.2010
+ * © 2007-2010 Alexander Egorov
+ */
+
+using MSBuild.TeamCity.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class TReportMessageBuilder
+	{
+		const string Text = "t";
+
+		[Test]
+		public void Full()
+		{
+			const string error = "e";
+			const string status = "ERROR";
+			
+			ReportMessageBuilder builder = new ReportMessageBuilder(Text, status, error);
+
+			Assert.That(builder.BuildMessage().ToString(), Is.EqualTo("##teamcity[message text='t' status='ERROR' errorDetails='e']"));
+		}
+
+		[Test]
+		public void StatusAndText()
+		{
+			const string status = "WARNING";
+			
+			ReportMessageBuilder builder = new ReportMessageBuilder(Text, status, null);
+			
+			Assert.That(builder.BuildMessage().ToString(), Is.EqualTo("##teamcity[message text='t' status='WARNING']"));
+		}
+
+		[Test]
+		public void OnlyText()
+		{
+			ReportMessageBuilder builder = new ReportMessageBuilder(Text, null, null);
+
+			Assert.That(builder.BuildMessage().ToString(), Is.EqualTo("##teamcity[message text='t']"));
+		}
+
+		[Test]
+		public void OnlyDetails()
+		{
+			const string error = "e";
+
+			ReportMessageBuilder builder = new ReportMessageBuilder(Text, null, error);
+
+			Assert.That(builder.BuildMessage().ToString(), Is.EqualTo("##teamcity[message text='t']"));
+		}
+	}
+}

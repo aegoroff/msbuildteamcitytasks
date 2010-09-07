0ae3f7d1b8f984d64a82bcf62c5efcb677fadfae
egoroff <devnull@localhost>
2010-09-07 08:17:37 +0000

    NCover3ReportTask implemented to manually Configuring Reporting Coverage using NCover 3 tool IssueID: 5

--- MSBuild.TeamCity.Tasks/DotNetCoverMessage.cs
+++ MSBuild.TeamCity.Tasks/DotNetCoverMessage.cs
@@ -4,6 +4,9 @@
  * © 2007-2010 Alexander Egorov
  */
 
+using System;
+using System.Collections.Generic;
+
 namespace MSBuild.TeamCity.Tasks
 {
 	/// <summary>
@@ -11,18 +14,42 @@ namespace MSBuild.TeamCity.Tasks
 	/// </summary>
 	public class DotNetCoverMessage : TeamCityMessage
 	{
+		internal const string NCover3HomeKey = "ncover3_home";
+		internal const string NCover3ReporterArgsKey = "ncover3_reporter_args";
+		internal const string NCoverExplorerToolKey = "ncover_explorer_tool";
+		internal const string NCoverExplorerToolArgsKey = "ncover_explorer_tool_args";
+		internal const string NCoverExplorerReportTypeKey = "ncover_explorer_report_type";
+		internal const string NCoverExplorerReportOrderKey = "ncover_explorer_report_order";
+		internal const string PartcoverReportXsltsKey = "partcover_report_xslts";
+
+		private readonly Dictionary<string, int> _validKeys = new Dictionary<string, int>(CreateValidKeys());
+
 		///<summary>
 		/// Initializes a new instance of the <see cref="DotNetCoverMessage"/> class
 		///</summary>
-		///<param name="value">key parameter value</param>
-		public DotNetCoverMessage( string value )
+		///<param name="key">Key's name</param>
+		///<param name="value">Parameter value</param>
+		/// <exception cref="ArgumentException">
+		/// Occurs in case of invalid key name
+		/// </exception>
+		public DotNetCoverMessage( string key, string value )
 		{
+			if ( !_validKeys.ContainsKey(key) )
+			{
+				throw new ArgumentException("Invalid key name.", "key");
+			}
+			Key = key;
 			Value = value;
-			Attributes.Add(new MessageAttributeItem("key", Value));
+			Attributes.Add(new MessageAttributeItem(Key, Value));
 		}
 
 		/// <summary>
 		/// Gets parameter value
+		/// </summary>
+		public string Key { get; private set; }
+
+		/// <summary>
+		/// Gets parameter key name
 		/// </summary>
 		public string Value { get; private set; }
 
@@ -32,6 +59,21 @@ namespace MSBuild.TeamCity.Tasks
 		protected override string Message
 		{
 			get { return "dotNetCoverage"; }
+		}
+
+		private static Dictionary<string, int> CreateValidKeys()
+		{
+			Dictionary<string, int> result = new Dictionary<string, int>
+			                                 	{
+			                                 		{ NCover3HomeKey, 1 },
+			                                 		{ NCover3ReporterArgsKey, 1 },
+			                                 		{ NCoverExplorerToolKey, 1 },
+			                                 		{ NCoverExplorerToolArgsKey, 1 },
+			                                 		{ NCoverExplorerReportTypeKey, 1 },
+			                                 		{ NCoverExplorerReportOrderKey, 1 },
+			                                 		{ PartcoverReportXsltsKey, 1 }
+			                                 	};
+			return result;
 		}
 	}
 }

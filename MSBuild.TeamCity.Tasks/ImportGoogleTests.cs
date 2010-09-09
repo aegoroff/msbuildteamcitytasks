d2182f59db2941be3ad00496fc1a239be8fdd3ae
egoroff <devnull@localhost>
2010-09-09 20:22:43 +0000

    ImportData and ImportGoogleTests tasks' tests added

--- MSBuild.TeamCity.Tasks/ImportGoogleTests.cs
+++ MSBuild.TeamCity.Tasks/ImportGoogleTests.cs
@@ -38,6 +38,23 @@ namespace MSBuild.TeamCity.Tasks
 	/// </example>
 	public class ImportGoogleTests : TeamCityTask
 	{
+		///<summary>
+		/// Initializes a new instance of the <see cref="ImportGoogleTests"/> class
+		///</summary>
+		public ImportGoogleTests()
+		{
+		}
+
+		///<summary>
+		/// Initializes a new instance of the <see cref="ImportGoogleTests"/> class using 
+		/// logger specified
+		///</summary>
+		///<param name="logger"><see cref="ILogger"/> implementation</param>
+		public ImportGoogleTests( ILogger logger )
+			: base(logger)
+		{
+		}
+
 		/// <summary>
 		/// Gets or sets full path to Google test xml output file
 		/// </summary>

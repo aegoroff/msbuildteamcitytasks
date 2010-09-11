b4b8a4dd9c9858d5733fbde0e928edd8c9e17ff0
egoroff <devnull@localhost>
2010-09-11 18:44:46 +0000

    Tasks default constructors tests to improve coverage

---
+++ src/Tests/TTaskDefaultCtor.cs
@@ -0,0 +1,31 @@
+using MSBuild.TeamCity.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture(typeof(BlockClose))]
+	[TestFixture(typeof(BlockOpen))]
+	[TestFixture(typeof(BuildNumber))]
+	[TestFixture(typeof(BuildProgressFinish))]
+	[TestFixture(typeof(BuildProgressMessage))]
+	[TestFixture(typeof(BuildProgressStart))]
+	[TestFixture(typeof(BuildStatus))]
+	[TestFixture(typeof(ImportData))]
+	[TestFixture(typeof(ImportGoogleTests))]
+	[TestFixture(typeof(NCover3Report))]
+	[TestFixture(typeof(NCoverReport))]
+	[TestFixture(typeof(PartCoverReport))]
+	[TestFixture(typeof(PublishArtifacts))]
+	[TestFixture(typeof(ReportBuildStatistic))]
+	[TestFixture(typeof(ReportMessage))]
+	[TestFixture(typeof(RunGoogleTests))]
+	[TestFixture(typeof(RunPartCoverage))]
+	public class TTaskDefaultCtor<TTsk> where TTsk: TeamCityTask, new()
+	{
+		[Test]
+		public void Create()
+		{
+			new TTsk();
+		}
+	}
+}

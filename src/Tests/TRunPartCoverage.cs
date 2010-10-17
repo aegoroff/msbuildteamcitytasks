861e808a53e9c36b55e53253088796a814626ef8
egr <devnull@localhost>
2010-10-17 18:07:35 +0400

    TRunPartCoverage refactoring: removed some code duplication and improving readability

--- src/Tests/TRunPartCoverage.cs
+++ src/Tests/TRunPartCoverage.cs
@@ -21,6 +21,8 @@ namespace Tests
         private ITaskItem _item2;
         private const string ItemSpec = "ItemSpec";
         private const string TargetArguments = "--help";
+        private const string TargetWorkDir = ".";
+        private RunPartCoverage _task;
 
 
         [SetUp]
@@ -29,75 +31,52 @@ namespace Tests
             Setup();
             _item1 = Mockery.NewMock<ITaskItem>();
             _item2 = Mockery.NewMock<ITaskItem>();
+            _task = new RunPartCoverage(Logger);
         }
 
         [Test]
         public void ToolPath()
         {
             Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
-
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            Assert.That(_task.Execute());
         }
 
         [Test]
         public void ToolPathInvalid()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = "bad"
-                                       };
-
-            Assert.That(task.Execute(), Is.False);
+            _task.ToolPath = "bad";
+            Assert.That(_task.Execute(), Is.False);
         }
 
         [Test]
         public void ToolPathAndTargetPath()
         {
             Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
-
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            Assert.That(_task.Execute());
         }
 
         [Test]
         public void ToolPathAndTargetPathAndTargetArguments()
         {
             Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
-
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath,
-                                           TargetArguments = TargetArguments
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            _task.TargetArguments = TargetArguments;
+            Assert.That(_task.Execute());
         }
 
         [Test]
         public void ToolPathAndTargetPathAndTargetArgumentsAndTargetWorkDir()
         {
             Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
-
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath,
-                                           TargetArguments = TargetArguments,
-                                           TargetWorkDir = ".",
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            _task.TargetArguments = TargetArguments;
+            _task.TargetWorkDir = TargetWorkDir;
+            Assert.That(_task.Execute());
         }
 
         [Test]
@@ -108,16 +87,12 @@ namespace Tests
             Expect.Once.On(_item1).GetProperty(ItemSpec).Will(Return.Value("a"));
             Expect.Once.On(_item2).GetProperty(ItemSpec).Will(Return.Value("b"));
 
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath,
-                                           TargetArguments = TargetArguments,
-                                           TargetWorkDir = ".",
-                                           Includes = new[] { _item1 }
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            _task.TargetArguments = TargetArguments;
+            _task.TargetWorkDir = TargetWorkDir;
+            _task.Includes = new[] { _item1 };
+            Assert.That(_task.Execute());
         }
 
         [Test]
@@ -128,16 +103,12 @@ namespace Tests
             Expect.Once.On(_item1).GetProperty(ItemSpec).Will(Return.Value("a"));
             Expect.Once.On(_item2).GetProperty(ItemSpec).Will(Return.Value("b"));
 
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath,
-                                           TargetArguments = TargetArguments,
-                                           TargetWorkDir = ".",
-                                           Excludes = new[] { _item2 }
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            _task.TargetArguments = TargetArguments;
+            _task.TargetWorkDir = TargetWorkDir;
+            _task.Excludes = new[] { _item1 };
+            Assert.That(_task.Execute());
         }
 
         [Test]
@@ -148,83 +119,55 @@ namespace Tests
             Expect.Once.On(_item1).GetProperty(ItemSpec).Will(Return.Value("a"));
             Expect.Once.On(_item2).GetProperty(ItemSpec).Will(Return.Value("b"));
 
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath,
-                                           TargetArguments = TargetArguments,
-                                           TargetWorkDir = ".",
-                                           Includes = new[] { _item1 },
-                                           Excludes = new[] { _item2 }
-                                       };
-
-            Assert.That(task.Execute());
+            _task.ToolPath = ValidPathToPartCover;
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            _task.TargetArguments = TargetArguments;
+            _task.TargetWorkDir = TargetWorkDir;
+            _task.Excludes = new[] { _item1 };
+            _task.Excludes = new[] { _item2 };
+            Assert.That(_task.Execute());
         }
 
         [Test]
         public void ToolPathProperty()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           ToolPath = ValidPathToPartCover,
-                                       };
-
-            Assert.That(task.ToolPath, Is.EqualTo(ValidPathToPartCover));
+            _task.ToolPath = ValidPathToPartCover;
+            Assert.That(_task.ToolPath, Is.EqualTo(ValidPathToPartCover));
         }
 
         [Test]
         public void TargetPathProperty()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           TargetPath = TGoogleTestsRunner.CorrectExePath,
-                                       };
-
-            Assert.That(task.TargetPath, Is.EqualTo(TGoogleTestsRunner.CorrectExePath));
+            _task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            Assert.That(_task.TargetPath, Is.EqualTo(TGoogleTestsRunner.CorrectExePath));
         }
 
         [Test]
         public void TargetArgumentsProperty()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           TargetArguments = TargetArguments,
-                                       };
-
-            Assert.That(task.TargetArguments, Is.EqualTo(TargetArguments));
+            _task.TargetArguments = TargetArguments;
+            Assert.That(_task.TargetArguments, Is.EqualTo(TargetArguments));
         }
 
         [Test]
         public void TargetWorkDirProperty()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           TargetWorkDir = ".",
-                                       };
-
-            Assert.That(task.TargetWorkDir, Is.EqualTo("."));
+            _task.TargetWorkDir = TargetWorkDir;
+            Assert.That(_task.TargetWorkDir, Is.EqualTo(TargetWorkDir));
         }
 
         [Test]
         public void IncludesProperty()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           Includes = new[] { _item1 }
-                                       };
-
-            Assert.That(task.Includes, Is.EquivalentTo(new[] { _item1 }));
+            _task.Includes = new[] { _item1 };
+            Assert.That(_task.Includes, Is.EquivalentTo(new[] { _item1 }));
         }
 
         [Test]
         public void ExcludesProperty()
         {
-            RunPartCoverage task = new RunPartCoverage(Logger)
-                                       {
-                                           Excludes = new[] { _item2 }
-                                       };
-
-            Assert.That(task.Excludes, Is.EquivalentTo(new[] { _item2 }));
+            _task.Excludes = new[] { _item2 };
+            Assert.That(_task.Excludes, Is.EquivalentTo(new[] { _item2 }));
         }
     }
 }

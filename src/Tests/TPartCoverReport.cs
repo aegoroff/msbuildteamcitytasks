f30c26228b0a2e7b166c9fdc95eaa1bd9c9fa898
egr <devnull@localhost>
2010-10-17 17:53:32 +0400

    TPartCoverReport refactoring: removed some code duplication and improving readability

--- src/Tests/TPartCoverReport.cs
+++ src/Tests/TPartCoverReport.cs
@@ -19,6 +19,8 @@ namespace Tests
         private ITaskItem _item1;
         private ITaskItem _item2;
         private const string ItemSpec = "ItemSpec";
+        private PartCoverReport task;
+        private const string XmlReportPth = "path";
 
 
         [SetUp]
@@ -27,17 +29,14 @@ namespace Tests
             Setup();
             _item1 = Mockery.NewMock<ITaskItem>();
             _item2 = Mockery.NewMock<ITaskItem>();
+            task = new PartCoverReport(Logger);
         }
 
         [Test]
         public void OnlyRequired()
         {
             Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
-
-            PartCoverReport task = new PartCoverReport(Logger)
-                                       {
-                                           XmlReportPath = "path"
-                                       };
+            task.XmlReportPath = XmlReportPth;
             Assert.That(task.Execute());
         }
 
@@ -47,32 +46,22 @@ namespace Tests
             Expect.Exactly(2).On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
             Expect.Exactly(2).On(_item1).GetProperty(ItemSpec).Will(Return.Value("a"));
             Expect.Exactly(2).On(_item2).GetProperty(ItemSpec).Will(Return.Value("b"));
-
-            PartCoverReport task = new PartCoverReport(Logger)
-                                       {
-                                           XmlReportPath = "path",
-                                           ReportXslts = new[] { _item1, _item2 }
-                                       };
+            task.XmlReportPath = XmlReportPth;
+            task.ReportXslts = new[] { _item1, _item2 };
             Assert.That(task.Execute());
         }
 
         [Test]
         public void XmlReportPath()
         {
-            PartCoverReport task = new PartCoverReport(Logger)
-                                       {
-                                           XmlReportPath = "path"
-                                       };
-            Assert.That(task.XmlReportPath, Is.EqualTo("path"));
+            task.XmlReportPath = XmlReportPth;
+            Assert.That(task.XmlReportPath, Is.EqualTo(XmlReportPth));
         }
 
         [Test]
         public void ReportXslts()
         {
-            PartCoverReport task = new PartCoverReport(Logger)
-                                       {
-                                           ReportXslts = new[] { _item1, _item2 }
-                                       };
+            task.ReportXslts = new[] { _item1, _item2 };
             Assert.That(task.ReportXslts, Is.EquivalentTo(new[] { _item1, _item2 }));
         }
     }

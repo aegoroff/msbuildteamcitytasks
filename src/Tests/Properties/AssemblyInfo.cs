1b1bd8152c7455e56412e9daed8177a3de70f323
egr <devnull@localhost>
2011-01-16 17:40:23 +0300

    Copyright actualized

--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexander Egorov")]
 [assembly: AssemblyProduct("MSBuild TeamCity Tasks Tests")]
-[assembly: AssemblyCopyright("Copyright © Alexander Egorov 2009-2010")]
+[assembly: AssemblyCopyright("Copyright © Alexander Egorov 2009-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

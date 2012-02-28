6ee396a061c912a491e65e305bc3a6d25b6d1daf
egorov <devnull@localhost>
2012-02-28 16:46:15 +0400

    refactoring: Block* tasks joined into single file

--- src/MSBuild.TeamCity.Tasks/BlockTask.cs
+++ src/MSBuild.TeamCity.Tasks/BlockTask.cs
@@ -4,7 +4,9 @@
  * © 2007-2012 Alexander Egorov
  */
 
+using System.Collections.Generic;
 using Microsoft.Build.Framework;
+using MSBuild.TeamCity.Tasks.Messages;
 
 namespace MSBuild.TeamCity.Tasks
 {
@@ -35,5 +37,95 @@ namespace MSBuild.TeamCity.Tasks
         /// </summary>
         [Required]
         public string Name { get; set; }
+    }
+
+    ///<summary>
+    /// Block opening message. Blocks are used to group several messages in the build log.
+    ///</summary>
+    /// <example>Opens block
+    /// <code><![CDATA[
+    /// <BlockOpen Name="b1" />
+    /// ]]></code>
+    /// Opens block full example (with all optional attributes)
+    /// <code><![CDATA[
+    /// <BlockOpen
+    ///     IsAddTimestamp="true"
+    ///     FlowId="1"
+    ///     Name="b1"
+    /// />
+    /// ]]></code>
+    /// </example>
+    public class BlockOpen : BlockTask
+    {
+        ///<summary>
+        /// Initializes a new instance of the <see cref="BlockOpen"/> class
+        ///</summary>
+        public BlockOpen()
+        {
+        }
+
+        ///<summary>
+        /// Initializes a new instance of the <see cref="BlockOpen"/> class using 
+        /// logger specified
+        ///</summary>
+        ///<param name="logger"><see cref="ILogger"/> implementation</param>
+        public BlockOpen(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        /// <summary>
+        /// Reads TeamCity messages
+        /// </summary>
+        /// <returns>TeamCity messages list</returns>
+        protected override IEnumerable<TeamCityMessage> ReadMessages()
+        {
+            yield return new BlockOpenTeamCityMessage(Name);
+        }
+    }
+
+    ///<summary>
+    /// Block closing message. Blocks are used to group several messages in the build log.
+    ///</summary>
+    /// <example>Closes block
+    /// <code><![CDATA[
+    /// <BlockClose Name="b1" />
+    /// ]]></code>
+    /// Closes block full example (with all optional attributes)
+    /// <code><![CDATA[
+    /// <BlockClose
+    ///     IsAddTimestamp="true"
+    ///     FlowId="1"
+    ///     Name="b1"
+    /// />
+    /// ]]></code>
+    /// </example>
+    public class BlockClose : BlockTask
+    {
+        ///<summary>
+        /// Initializes a new instance of the <see cref="MSBuild.TeamCity.Tasks.BlockClose"/> class
+        ///</summary>
+        public BlockClose()
+        {
+        }
+
+        ///<summary>
+        /// Initializes a new instance of the <see cref="MSBuild.TeamCity.Tasks.BlockClose"/> class using 
+        /// logger specified
+        ///</summary>
+        ///<param name="logger"><see cref="ILogger"/> implementation</param>
+        public BlockClose(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        /// <summary>
+        /// Reads TeamCity messages
+        /// </summary>
+        /// <returns>TeamCity messages list</returns>
+        protected override IEnumerable<TeamCityMessage> ReadMessages()
+        {
+            yield return new BlockCloseTeamCityMessage(Name);
+        }
     }
 }

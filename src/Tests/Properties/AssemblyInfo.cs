31a889e1f5e83392e5a299c6fd57823790551da1
egorov <devnull@localhost>
2012-02-28 11:17:38 +0400

    cosmetics: Assembly copyright actualized

--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("Alexander Egorov")]
 [assembly : AssemblyProduct("MSBuild TeamCity Tasks Tests")]
-[assembly : AssemblyCopyright("Copyright © Alexander Egorov 2009-2011")]
+[assembly : AssemblyCopyright("Copyright © Alexander Egorov 2009-2012")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
 

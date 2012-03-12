2b2c676666992db302d3817b6e6c0f044021c444
egr <devnull@localhost>
2012-03-12 21:26:09 +0400

    Filter and all properties tests

--- src/Tests/TRunOpenCoverage.cs
+++ src/Tests/TRunOpenCoverage.cs
@@ -5,6 +5,7 @@
  */
 
 using MSBuild.TeamCity.Tasks;
+using Microsoft.Build.Framework;
 using NMock2;
 using NUnit.Framework;
 using Is = NUnit.Framework.Is;
@@ -18,6 +19,11 @@ namespace Tests
 
         private RunOpenCoverage task;
         private const string ValidPathToOpenCover = @"C:\Program Files (x86)\OpenCover";
+        private ITaskItem item1;
+        private ITaskItem item2;
+        private const string ItemSpec = "ItemSpec";
+        private const string TargetArguments = "--help";
+        private const string TargetWorkDir = ".";
 
         #endregion
 
@@ -27,6 +33,8 @@ namespace Tests
         public void ThisSetup()
         {
             Setup();
+            item1 = Mockery.NewMock<ITaskItem>();
+            item2 = Mockery.NewMock<ITaskItem>();
             task = new RunOpenCoverage(Logger);
         }
 
@@ -52,6 +60,29 @@ namespace Tests
             Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
             task.ToolPath = ValidPathToOpenCover;
             task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            Assert.That(task.Execute());
+        }
+
+        [Test]
+        public void FilterProperty()
+        {
+            task.Filter = new[] { item1 };
+            Assert.That(task.Filter, Is.EquivalentTo(new[] { item1 }));
+        }
+
+        [Test]
+        public void AllProperties()
+        {
+            Expect.Once.On(Logger).Method(TTeamCityTaskImplementation.LogMessage).WithAnyArguments();
+
+            Expect.Once.On(item1).GetProperty(ItemSpec).Will(Return.Value("a"));
+            Expect.Once.On(item2).GetProperty(ItemSpec).Will(Return.Value("b"));
+
+            task.ToolPath = ValidPathToOpenCover;
+            task.TargetPath = TGoogleTestsRunner.CorrectExePath;
+            task.TargetArguments = TargetArguments;
+            task.TargetWorkDir = TargetWorkDir;
+            task.Filter = new[] { item1, item2 };
             Assert.That(task.Execute());
         }
 

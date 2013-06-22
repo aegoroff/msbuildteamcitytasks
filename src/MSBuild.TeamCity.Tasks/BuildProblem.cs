166c66c25f73befa5877afc792d056519d204ec0
egoroff <egoroff@gmail.com>
2013-06-22 10:43:33 +0400

    Uploading removed from build script

---
+++ src/MSBuild.TeamCity.Tasks/BuildProblem.cs
@@ -0,0 +1,12 @@
+﻿// /*
+//  * Created by: egr
+//  * Created at: 28.10.2007
+//  * © 2007-2008 Alexander Egorov
+//  */
+namespace MSBuild.TeamCity.Tasks
+{
+    public class BuildProblem
+    {
+         
+    }
+}

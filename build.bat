4ee803f88f02a51d2ec29fe391d1ae568f2fc0b4
egoroff <egoroff@gmail.com>
2015-05-15 21:29:09 +0300

    Build parameters passing

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
-msbuild src\MSBuild.TeamCity.Tasks.xml
+msbuild src\MSBuild.TeamCity.Tasks.xml /p:Configuration=Release;Platform=AnyCPU
